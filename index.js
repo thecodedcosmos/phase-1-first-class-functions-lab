5860990121297b00c152cf362bad21538fa3b906
adriannahdbell@gmail.com <120890577+thecodedcosmos@users.noreply.github.com>
2023-09-28 23:00:13 -0700

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+  return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function(drivers) {
+  return drivers.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(x) {
+    return function (fare) {
+        return x * fare
+};
+    }
+function fareDoubler(fare) {
+    return fare * 2
+};
+
+function fareTripler(fare) {
+    return fare * 3
+};
+
+function selectDifferentDrivers(drivers, returnLastTwoDrivers) {
+  const lastTwoDrivers = returnLastTwoDrivers(drivers);
+  return lastTwoDrivers;
+}
